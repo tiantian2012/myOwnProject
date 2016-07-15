57e082e73b87a98da7674a191a2cf4ae277e5750
tiantian2012 <1024074995@qq.com>
2016-07-15 18:40:37 +0800

    https://github.com/tiantian2012/myOwnProject.git

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import {Component,Input} from '@angular/core';
+import '../../public/css/styles.css';
+import {HeroDetailComponent} from './hero-detail.component'
+import {Hero} from './hero'
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  directives: [HeroDetailComponent]
+})
+export class AppComponent {
+  
+  title = 'Tour of Heroes!';
+  public heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+
+
+}
+
+
+
+
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
