8cc466613b76bedb4164c756ed6826cc356cd0a7
tiantian2012 <1024074995@qq.com>
2016-07-17 00:23:24 +0800

    https://github.com/tiantian2012/myOwnProject.git

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
+import { HTTP_PROVIDERS } from '@angular/http';
+
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
 bootstrap(AppComponent, [
-  APP_ROUTER_PROVIDERS
+  APP_ROUTER_PROVIDERS,
+  HTTP_PROVIDERS
 ]);
